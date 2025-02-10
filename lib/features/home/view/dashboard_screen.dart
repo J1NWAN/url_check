40968304dc9e4b51fd519be12f26baa651e20992
j1nwan <weamicron@gmail.com>
2025-02-10 21:19:06 +0900

    design: Home 화면 디자인 수정
    - PageView 적용
    - 페이징 방향 수직으로 설정

--- lib/features/home/view/dashboard_screen.dart
+++ lib/features/home/view/dashboard_screen.dart
@@ -5,8 +5,91 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Dashboard')),
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('대시보드', style: Theme.of(context).textTheme.displaySmall),
+            Row(
+              children: [
+                IconButton(onPressed: () {}, icon: const Icon(Icons.upload_file_outlined)),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.download)),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
+              ],
+            )
+          ],
+        ),
+        const SizedBox(height: 20),
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 1,
+                      blurRadius: 10,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(onPressed: () {}, child: const Text('오늘')),
+                    TextButton(onPressed: () {}, child: const Text('주')),
+                    TextButton(onPressed: () {}, child: const Text('월')),
+                    TextButton(onPressed: () {}, child: const Text('년')),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Container(
+          constraints: const BoxConstraints(
+            minHeight: 300,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 1,
+                blurRadius: 10,
+              ),
+            ],
+          ),
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.red,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:url_check/core/button/custom_button.dart';
+import 'package:url_check/features/home/view/dashboard_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,97 +8,16 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: double.infinity,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('대시보드', style: Theme.of(context).textTheme.displaySmall),
-                    Row(
-                      children: [
-                        IconButton(onPressed: () {}, icon: const Icon(Icons.upload_file_outlined)),
-                        IconButton(onPressed: () {}, icon: const Icon(Icons.download)),
-                        IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.3),
-                              spreadRadius: 1,
-                              blurRadius: 10,
-                              offset: const Offset(0, 3),
-                            ),
-                          ],
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            TextButton(onPressed: () {}, child: const Text('오늘')),
-                            TextButton(onPressed: () {}, child: const Text('주')),
-                            TextButton(onPressed: () {}, child: const Text('월')),
-                            TextButton(onPressed: () {}, child: const Text('년')),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  constraints: const BoxConstraints(
-                    minHeight: 300,
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 1,
-                        blurRadius: 10,
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        height: 300,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.red,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Container(
-                        width: double.infinity,
-                        height: 300,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+      body: SizedBox.expand(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: PageView(
+            scrollDirection: Axis.vertical,
+            children: const [
+              DashboardScreen(),
+              DashboardScreen(),
+              DashboardScreen(),
+            ],
           ),
         ),
       ),
