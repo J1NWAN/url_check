b59d4d797e971560e26dc0654328517cacebdc8a
j1nwan <weamicron@gmail.com>
2025-02-18 21:00:13 +0900

    feat: 로케일 설정

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:url_check/core/theme/theme_view_model.dart';
 import 'package:url_check/core/router/app_router.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class UrlCheckerApp extends ConsumerWidget {
   const UrlCheckerApp({super.key});
@@ -15,6 +16,14 @@ class UrlCheckerApp extends ConsumerWidget {
       title: 'URL Checker',
       theme: themeState.themeData,
       routerConfig: router,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ko', 'KR'),
+      ],
     );
   }
 }
