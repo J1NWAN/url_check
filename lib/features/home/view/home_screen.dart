79cd3e2a2e698411468a41744e5c71fece9f7a04
j1nwan <weamicron@gmail.com>
2025-02-21 21:25:29 +0900

    style: 불필요한 임포트 삭제

--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:url_check/core/layout/main_layout.dart';
 import 'package:url_check/features/home/view/dashboard_screen.dart';
 import 'package:url_check/features/home/view/url_analysis_screen.dart';
 import 'package:url_check/features/home/viewmodel/home_view_model.dart';
