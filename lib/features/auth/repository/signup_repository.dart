64db95d3cabc40e634ebcb3a9d82f4178de75196
j1nwan <weamicron@gmail.com>
2025-03-12 21:47:47 +0900

    feat: 비밀번호 암호화 및 중복 아이디 체크 기능 추가

--- lib/features/auth/repository/signup_repository.dart
+++ lib/features/auth/repository/signup_repository.dart
@@ -4,6 +4,12 @@ import 'package:url_check/features/auth/model/user_model.dart';
 class SignupRepository {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  /// ************* 아이디 중복 체크 **************
+  Future<bool> checkId(String id) async {
+    final snapshot = await firestore.collection('users').doc(id).get();
+    return snapshot.exists;
+  }
+
   /// ************* 회원가입 **************
   Future<void> signup(UserModel user) async {
     await firestore.collection('users').doc(user.id).set(user.toJson());
--- lib/features/auth/viewmodel/signup_view_model.dart
+++ lib/features/auth/viewmodel/signup_view_model.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:url_check/core/toast/custom_toast.dart';
@@ -39,9 +41,20 @@ class SignupViewModel extends _$SignupViewModel {
     );
   }
 
-  void signup(BuildContext context) {
+  void signup(BuildContext context) async {
     if (state.idController.text.isEmpty) {
       CustomToast.show(context, '아이디를 입력해주세요', type: ToastType.error);
+      return;
+    }
+
+    if (state.idController.text.length < 4) {
+      CustomToast.show(context, '아이디는 4자 이상이어야 합니다', type: ToastType.error);
+      return;
+    }
+
+    bool isIdExists = await signupRepository.checkId(state.idController.text);
+    if (isIdExists) {
+      CustomToast.show(context, '이미 존재하는 아이디입니다', type: ToastType.error);
       return;
     }
 
@@ -55,11 +68,15 @@ class SignupViewModel extends _$SignupViewModel {
       return;
     }
 
+    // 비밀번호 암호화
+    var bytes = utf8.encode(state.passwordController.text);
+    var passwordHash = sha256.convert(bytes).toString();
+
     // 회원가입 로직 구현
     final user = UserModel(
       id: state.idController.text,
       name: state.nameController.text,
-      password: state.passwordController.text,
+      password: passwordHash,
       email: state.emailController.text,
       createdAt: DateTime.now(),
       updatedAt: DateTime.now(),
@@ -69,7 +86,6 @@ class SignupViewModel extends _$SignupViewModel {
 
     // 성공 시 로그인 화면으로 이동
     CustomToast.show(context, '회원가입이 완료되었습니다', type: ToastType.success);
-
     context.go('/login');
   }
 }
